09e24ff21f20701919e8bf67ec3bd34391491c43
Divvyeash Kumarasen <kumarasendivvyeash@gmail.com>
2026-05-14 22:37:46 +0800

    Fixed missing db import in models

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template, request, redirect, url_for, jsonify
+from models import db, User
+from werkzeug.security import generate_password_hash, check_password_hash
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///crewfinder.db'
+app.config['SECRET_KEY'] = 'mmu_it_project_2026'
+db.init_app(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        # Handles both JSON (Shivanie) and Form data (Ransly)
+        data = request.get_json() if request.is_json else request.form
+        
+        email = data.get('email')
+        if User.query.filter_by(email=email).first():
+            return jsonify({"message": "Email already exists"}), 400
+            
+        hashed_pw = generate_password_hash(data.get('password'))
+        new_user = User(username=data.get('username'), email=email, password=hashed_pw)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for('login'))
+    return render_template('register.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
